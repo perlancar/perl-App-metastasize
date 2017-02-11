b827d6df32178873d1cdb11ed64c756901b40799
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-02-11 08:34:52 +0700

    Initial commit

---
+++ lib/App/metastasize.pm
@@ -0,0 +1,11 @@
+package App::metastasize;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: Rename functions and variables in a Perl script/module using metasyntactic/random variables
+
+=head1 SYNOPSIS
+
+Use the included script L<metastasize>.
